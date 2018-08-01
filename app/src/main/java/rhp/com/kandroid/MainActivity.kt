f0400edfe0b6a004b5a7e03fcf281b8de4e9a55c
ravihp <ravi.pandit@vizio.com>
2018-07-31 18:50:06 -0700

    Added Kotlin Android files.

---
+++ app/src/main/java/rhp/com/kandroid/MainActivity.kt
@@ -0,0 +1,13 @@
+
+package rhp.com.kandroid
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
